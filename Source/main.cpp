82a733527cfebe94517334abc0e251d98f50a938
phamhuyen284 <huyenpham26112001@gmail.com>
2022-05-29 08:32:40 +0700

    huyen done

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,51 +5,63 @@
 
 static int day= 0;
 GLfloat t=1.0f;  
-GLfloat t1=1.0f,t2=1.0f,t3=-2.5f;
+GLfloat t1=0.0f,t2=0.0f,t3=0.0f;
 GLfloat windowWidth;
 GLfloat windowHeight;
 GLfloat xstep=0.1f; 
 GLfloat ystep=0.1f; 
+// thuoc tinh cho nguon sang chieu vao qua bong
+GLfloat light_ambientball[]={0.0,1.0,1.0,1.0};
+GLfloat light_diffuseball[]={0.0,1.0,1.0,1.0};
+// thuoc tinh cho nguon sang chieu vao thanh truot
+GLfloat light_ambientThanh[]={1.0,0.5,1.0,1.0};
+GLfloat light_diffuseThanh[]={1.0,0.5,1.0,1.0}; 
+
 void display(void)
 {
-	glClear (GL_COLOR_BUFFER_BIT);
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 //  Ve mat phang
 	glPushMatrix();
-	glColor3f(0.0,1.0,0.0);
-	glScalef(5,0.6,5);
-	glTranslatef((GLfloat) day/5 , -9, 0);	
-	glutSolidCube(1.0);
+		glColor3f(0.0,1.0,0.0);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambientThanh);
+ 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuseThanh);
+		glScalef(5,1,5);
+		glTranslatef((GLfloat) day/5 , -5, 0);	
+		glutSolidCube(1.0);
 	glPopMatrix();
-	
-//	gluLookAt (5.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, .0);	
+//  ve qua bong
 	glPushMatrix();
 		glColor3f (1.0, 0.5, 0.0);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambientball);
+ 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuseball);
 		glTranslatef(t1,t2,t3); 
-		glutSolidSphere(1.0,200.0,200.0); 
+		glutSolidSphere(1.0,30.0,30.0); 
 	glPopMatrix(); 
 	glutSwapBuffers();
 }
+
 // xu ly bong
 void TimerFunctionForObject(int value)
-    {   	
+    { 
     	t1+=xstep;
 		t2+=ystep;
+		// bat dieu kien de bong di chuyen
 		if(t1>=10 || t1<=-10)
 			 xstep=-xstep;
 		if(t2>=5 )
 			ystep=-ystep;  
-//			t2<=0+1
-		if(t2<=-5){
-			if(t1>= (GLfloat) day/5 && t1<= (GLfloat) day/5+5 )
+		if(t2<=-3.5 ){
+			if(t1> (GLfloat) day-2.5 && t1< (GLfloat) day+2.5 ){// bat dieu kien de bong cham vao thanh truot se bat len
 				ystep=-ystep; 
+				t2=-3.5;
+			}
 		}
-			 
-			
-//		|| t2<=-5    
 		
-//		t1+=xstep;
-//		t2+=ystep;    
+		// bat dieu kien cho game ket thuc
+		if( t2<-3.5)
+			exit(0);
+	  
     glutPostRedisplay();
     glutTimerFunc(30,TimerFunctionForObject, 1);
 }
@@ -64,19 +76,15 @@ void SetupRC(void)
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
     }
 
-
- 
-
 void reshape (int w, int h)
 {
 	glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
 	glMatrixMode (GL_PROJECTION);
 	glLoadIdentity ();
-	gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+	gluPerspective(80.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt (0, 1, 10, 0.0, 0.0, 0.0, 0, 1, 0);
-//	gluLookAt (5.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, .0);	
 }
 
 void limitLeft() {
@@ -120,7 +128,20 @@ void keyboard (int key, int x, int y)
 
 void init(void) 
 {
+
+	GLfloat mat_specular[] = { 0.7, 0.7, 0.7, 1.0 };
+	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
 	glClearColor (1.0, 1.0, 1.0, 1.0);
+ 
+	glShadeModel (GL_SMOOTH);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	 
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
 	glShadeModel (GL_FLAT);
 }
 
@@ -130,8 +151,8 @@ int main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize (1000, 1000); 
-	glutInitWindowPosition (0, 0);
-	glutCreateWindow (argv[0]);
+	glutInitWindowPosition (100, 100);
+	glutCreateWindow("Game");
 	init ();
 	glutDisplayFunc(display); 
 	glutReshapeFunc(reshape);
