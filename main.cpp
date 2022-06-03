e9bd44d3786142725dfdba5063b666a797d943c7
shiba20212021 <shiba2021shiba2021@gmail.com>
2022-06-03 23:30:06 +0700

    refactor

--- main.cpp
+++ main.cpp
@@ -3,42 +3,61 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <iostream>
-#include <string.h> 
 #include <string>
 #include <cstdlib>
 #include <sstream>
 #include <ctime>
 
+#define LIMITED_MOVE 8
+#define LIMITED_Y_COORDINATE 5
+
+#define WIDTH_WINDOW 500
+#define HEIGHT_WINDOW 500
+#define TITLE_GAME "Bouncing Ball Game"
+
+#define IS_START_GAME 0
+#define IS_PLAY_GAME 1
+#define IS_STOP_GAME 2
+
+
 using namespace std;
 
-static int day= 0;
-GLfloat t=1.0f;  
-GLfloat t1=0.0f,t2=0.0f,t3=0.0f;
+// Set up for circle
+GLfloat x = 1.0f;  
+GLfloat x1 = 0.0f, x2 = 0.0f, x3 = 0.0f;
+GLfloat xstep = 0.1f; 
+GLfloat ystep = 0.1f; 
+
+// Set up for bar
+int distanceMoveBar = 0;
+int barScaleX = 5;
+int barScaleY = 1;
+int barScaleZ = 5;
+
+// Window
 GLfloat windowWidth;
 GLfloat windowHeight;
-GLfloat xstep=0.1f; 
-GLfloat ystep=0.1f; 
-int check = 0;
-int score = 0;
-// thuoc tinh cho nguon sang chieu vao qua bong
-GLfloat light_ambientball[]={0.0,1.0,1.0,1.0};
-GLfloat light_diffuseball[]={0.0,1.0,1.0,1.0};
-// thuoc tinh cho nguon sang chieu vao thanh truot
-GLfloat light_ambientThanh[]={1.0,0.5,1.0,1.0};
-GLfloat light_diffuseThanh[]={1.0,0.5,1.0,1.0}; 
 
-//khai bao ham
-void welcomeDisplay();
-void endDisplay();
-//khai bao bien ham mytime
+// Set up for light ball 3D
+GLfloat light_ambient_ball[] = {0.0,1.0,1.0,1.0};
+GLfloat light_diffuse_ball[] = {0.0,1.0,1.0,1.0};
+// Set up for light bar 3D
+GLfloat light_ambient_bar[] = {1.0,0.5,1.0,1.0};
+GLfloat light_diffuse_bar[] = {1.0,0.5,1.0,1.0}; 
+
+int user_control = IS_START_GAME;
+int score = 0;
 
 void reset(){
-	t=1.0f;
-	t1=0.0f;t2=0.0f;t3=0.0f;
+	x = 1.0f;
+	x1 = 0.0f;
+	x2 = 0.0f;
+	x3 = 0.0f;
+	
 	xstep=0.1f;
 	ystep=0.1f;
 	score = 0;
-	day = 0;
+	distanceMoveBar = 0;
 }
 
 void welcomeDisplay() {
@@ -49,7 +68,7 @@ void welcomeDisplay() {
 	for(int i=0;i<strlen(msg);i++) {
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,msg[i]); 
 	}
-	
+
 	glRasterPos3f(0,0,0);
 	char msg1[] ="WELCOME BAll";
 	for(int i=0;i<strlen(msg1);i++) {
@@ -69,7 +88,7 @@ void welcomeDisplay() {
 	for(int i=0;i<strlen(msg4);i++) {
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,msg4[i]); 
 	}
-	
+
 	glutSwapBuffers();
 }
 
@@ -81,18 +100,18 @@ void endDisplay() {
 	for(int i=0;i<strlen(msg);i++) {
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,msg[i]); 
 	}
-	
+
 	string result;          
 	ostringstream convert;
 	convert << score;      
 	result = "Your score is " + convert.str() +" ^^";
-	
+
 	glColor3f(1.0,0.0,0);
 	glRasterPos3f(-0.8,-1.5,0);
 	for(int i=0;i<result.length();i++) {
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,result[i]); 
 	}
-	
+
 	glRasterPos3f(-0.8,-3,0);
 	char msg1[] ="GOODBYE BAll";
 	for(int i=0;i<strlen(msg1);i++) {
@@ -103,76 +122,92 @@ void endDisplay() {
 
 void display(void)
 {
-	if(check==1){
+	if(user_control == IS_PLAY_GAME) {
 		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-	  //Ve mat phang
+	
+	//  Draw and shading for ball
 		glPushMatrix();
 			glColor3f(0.0,1.0,0.0);
-			glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambientThanh);
-	 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuseThanh);
-			glScalef(5,1,5);
-			glTranslatef((GLfloat) day/5 , -5, 0);	
+			
+			glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient_ball);
+	 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse_ball);
+	 		
+			glScalef(barScaleX, barScaleY, barScaleZ);
+			glTranslatef((GLfloat) distanceMoveBar/5 , -5, 0);
+				
 			glutSolidCube(1.0);
 		glPopMatrix();
-	//  ve qua bong
+		
+	//  Draw and shading for bar
 		glPushMatrix();
 			glColor3f (1.0, 0.5, 0.0);
-			glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambientball);
-	 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuseball);
-			glTranslatef(t1,t2,t3); 
-			glutSolidSphere(1.0,30.0,30.0);
+			
+			glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient_bar);
+	 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse_bar);
+	 		
+			glTranslatef(x1, x2, x3);
+			
+			glutSolidSphere(1.0, 30.0, 30.0); 
 		glPopMatrix(); 
-		
 		glutSwapBuffers();
-	}else if(check==0){
+	}
+	
+	else if(user_control == IS_START_GAME) {
 		welcomeDisplay();
-	}else if (check == 2){
+	}
+	
+	else if(user_control == IS_STOP_GAME) {
 		endDisplay();
 	}
 }
 
-// xu ly bong
-void TimerFunctionForObject(int value)
-    { 
-    	if(check==1){
-	    	t1+=xstep;
-			t2+=ystep;
-			// bat dieu kien de bong di chuyen
-			if(t1>=10 || t1<=-10)
-				 xstep=-xstep;
-			if(t2>=5 )
-				ystep=-ystep;  
-			if(t2<=-3.5 ){
-				if(t1> (GLfloat) day-2.5 && t1< (GLfloat) day+2.5 ){// bat dieu kien de bong cham vao thanh truot se bat len
-					ystep=-ystep; 
-					t2=-3.5;
-					score += 1;
-				}
-			}
-			
-			// bat dieu kien cho game ket thuc
-			if( t2<-3.5){
-				check = 2;
-				glutPostRedisplay();
-			}
-	    	glutPostRedisplay();
-	    	glutTimerFunc(30,TimerFunctionForObject, 1);
-	    }else if (check == 0 || check == 2){
-	    	glutPostRedisplay();
-	    	glutTimerFunc(1000, TimerFunctionForObject, 0);
+// Handle ball for collide
+void timerFunctionForObject(int value)
+{ 
+	if(user_control == IS_PLAY_GAME) {
+	    x1 += xstep;
+		x2 += ystep;
+	
+		if(x1 >= LIMITED_MOVE || x1 <= -LIMITED_MOVE) {
+			xstep = -xstep;
 		}
+			
+		if(x2 >= LIMITED_Y_COORDINATE) {
+			ystep = -ystep;  
+		}
+			
+		int spaceCoordinate = LIMITED_Y_COORDINATE - barScaleY;
+		if(x2 <= -spaceCoordinate) {
+			GLfloat frontBarCoordinate = distanceMoveBar - (barScaleX / 2.0);
+			GLfloat backBarCoordinate = distanceMoveBar + (barScaleX / 2.0);
+			
+			if(x1 > frontBarCoordinate && x1 < backBarCoordinate){
+				ystep = -ystep; 
+				x2 = -spaceCoordinate;
+				
+				score++;
+			}
+		}
+		if(x2 < -spaceCoordinate) {
+			user_control = IS_STOP_GAME;
+			glutPostRedisplay();
+		}
+	
+	    glutPostRedisplay();
+	    glutTimerFunc(30, timerFunctionForObject, 1);
+	}
+	else if (user_control == IS_START_GAME || user_control == IS_STOP_GAME){
+	    glutPostRedisplay();
+	    glutTimerFunc(1000, timerFunctionForObject, 0);
+	}
 }
-
-
-
 
 // Setup the rendering state
 void SetupRC(void)
-    {
+{
     // Set clear color to blue
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-    }
+}
 
 void reshape (int w, int h)
 {
@@ -186,25 +221,24 @@ void reshape (int w, int h)
 }
 
 void limitLeft() {
-	if(day <= -10)
-		day = -10;
+	if(distanceMoveBar <= - LIMITED_MOVE)
+		distanceMoveBar = - LIMITED_MOVE;
 }
 
 void limitRight() {
-	if(day >= 10)
-		day = 10;
+	if(distanceMoveBar >= LIMITED_MOVE)
+		distanceMoveBar = LIMITED_MOVE;
 }
 
 void moveLeft() {
-	day = (day - 1); // di chuyen
+	distanceMoveBar = (distanceMoveBar - 1); // di chuyen
 	limitLeft();
 }
 
 void moveRight() {
-	day = (day + 1); // di chuyen
+	distanceMoveBar = (distanceMoveBar + 1); // di chuyen
 	limitRight();
 }
-
 
 void keyboard (int key, int x, int y)
 {
@@ -218,69 +252,49 @@ void keyboard (int key, int x, int y)
 			glutPostRedisplay();
 			break;
 		case GLUT_KEY_HOME: // (HOME)
-			if(check==2){
-				check = 0;
-				reset();
-				glutPostRedisplay();
-			}
-			break;
-		default:
-			break;
-	}
-}
-
-void keyboardchar (unsigned char key, int x, int y)
-{
-	switch (key) {
-		case 's':
-			check = 0;
+			user_control = IS_START_GAME;
 			reset();
 			glutPostRedisplay();
 			break;
+		default:
+			break;
+	}
+}
+
+void userKeyboard (unsigned char key, int x, int y)
+{
+	switch (key) {
+		case 's':
+			user_control = IS_START_GAME;
+			reset();
+			break;
 		case 'p':
-			if(check==0){
-				check = 1;
-				glutPostRedisplay();
+			if(user_control == IS_START_GAME){
+				user_control = IS_PLAY_GAME;
 			}
 			break;
 		case 'e':
-			if(check==0 || check==1){
-				check = 2;
-				glutPostRedisplay();
-			}
+			user_control = IS_STOP_GAME;
 			break;
 		default:
 			break;
 	}
-
-
-//	if(key == GLUT_KEY_HOME){
-//		check = 0;
-//			reset();
-//			glutPostRedisplay();
-//	}
-//	if(key== GLUT_KEY_END){
-//		if(check==0){
-//				check = 1;
-//				glutPostRedisplay();
-//		}
-//	}
+	glutPostRedisplay();
 }
-
- 
 
 void init(void) 
 {
+
 	GLfloat mat_specular[] = { 0.7, 0.7, 0.7, 1.0 };
 	GLfloat mat_shininess[] = { 50.0 };
 	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
 	glClearColor (1.0, 1.0, 1.0, 1.0);
- 
+
 	glShadeModel (GL_SMOOTH);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-	 
+
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
@@ -289,19 +303,18 @@ void init(void)
 
 
 int main(int argc, char** argv)
-{
+{		
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize (500, 500);
-	 
-	glutInitWindowPosition (100, 100);
-	glutCreateWindow("Game");
+	glutInitWindowSize (WIDTH_WINDOW, HEIGHT_WINDOW); 
+	glutInitWindowPosition (0, 0);
+	glutCreateWindow(TITLE_GAME);
 	init ();
 	glutDisplayFunc(display); 
 	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboardchar);
 	glutSpecialFunc(keyboard);
-	glutTimerFunc(500, TimerFunctionForObject, 0);
+	glutKeyboardFunc(userKeyboard);
+	glutTimerFunc(33, timerFunctionForObject, 1);
 	glutMainLoop();
 	return 0;
 }
